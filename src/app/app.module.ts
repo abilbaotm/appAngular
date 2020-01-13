e802f89d9d5daa9a4734ebb8adbd3798a834ee61
Asier Bilbao <abilbaotm@gmail.com>
2020-01-13 13:48:42 +0100

    añadir nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutes} from './app.routes';
+import { HeroesComponent } from './components/heroes/heroes.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
