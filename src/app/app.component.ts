cfec4d9e2cf768f89c3efea3f0cc5cf86ecb9f9d
Asier Bilbao <abilbaotm@gmail.com>
2020-01-18 02:05:18 +0100

    ejercicio7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import listadePostres from 'src/assets/json/postres.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EJ07-appJsonTabla';
+
+  Postres: any = listadePostres;
+
 }
